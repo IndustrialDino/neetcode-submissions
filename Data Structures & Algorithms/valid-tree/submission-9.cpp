0c15aeb71d3f4fb55e5d3065d289208423f1b972
IndustrialDino <kpaga.dev@gmail.com>
2026-08-16 16:47:41 -0700

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.cpp
@@ -0,0 +1,42 @@
+class DSU {
+    public:
+    std::unordered_map<int, int> parent;
+
+    int find(int node){
+        if (parent[node] == node) return node;
+
+        return find(parent[node]);
+    }
+
+    bool unite(int a, int b){
+        int root_a = find(a);
+        int root_b = find(b);
+
+        if (root_a == root_b) return false;
+
+        parent[root_a] = root_b;
+
+        return true;
+    }
+};
+
+class Solution {
+public:
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        DSU dsu;
+        for (int i = 0; i < n; i++){
+            dsu.parent[i] = i;
+        }
+
+        for (const auto& edge: edges){
+            bool failed = dsu.unite(edge[0], edge[1]); // fail for cycle
+
+            if (failed) return false;
+        }
+
+        return true; // to check visited nodes and full connectedness
+    }
+
+
+};
