c4c8b4cc39c0c4f1272af35cdc896c23d9aa2229
IndustrialDino <kpaga.dev@gmail.com>
2026-08-27 20:13:26 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,38 @@
+#include <sstream>
+class Solution {
+public:
+    char getLowerChar(const char& c) const {
+        if (c >= 'A' && c <= 'A') return 'a' + c - 'A';
+
+        return c;
+    }
+    std::string getHash(const std::string& str) const{
+        std::array<int, 256> countT{};
+        for (const auto& c: str) {
+            int val  = getLowerChar(c) - 'a';
+            countT[val]++;
+        }
+
+        std::stringstream ss;
+        for (const auto& val : countT){
+            ss << val;
+        }
+
+        return ss.str();
+    } 
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> hash_map;
+        
+        for (const auto& str: strs){
+            hash_map[getHash(str)].push_back(str);
+        }
+
+
+        std::vector<std::vector<std::string>> result;
+        for (const auto& [key, val] : hash_map){
+            result.push_back(val);
+        }
+
+        return result;
+    }
+};
